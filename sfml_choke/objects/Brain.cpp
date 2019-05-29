42609cb2e2d846aba923ca007fcbebc53886a45c
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-05-29 17:50:27 +0300

    added game units brain logic

--- sfml_choke/objects/Brain.cpp
+++ sfml_choke/objects/Brain.cpp
@@ -2,9 +2,153 @@
 #include "Brain.hpp"
 
 namespace GO {
-  Brain::Brain(const GameUnit* owner)
+
+  //private:
+  //  const GameUnit* owner;
+  //  enum states {attack, patrol, stand};
+  //  int currState;
+  //
+  //protected:
+  //  void checkHazards();
+  //  void checkHazardsByLeftSide();
+  //  void checkHazardsByRightSide();
+  //  void handleCollision();
+  //  void detectPlayer();
+  //
+  //public:
+  //  Brain(const GameUnit* owner);
+  //  void think(const std::vector<std::string>&);
+  //};
+  
+  
+  Brain::Brain(GameUnit* owner)
   {
     this->owner = owner;
   }
   
+  void Brain::think(const std::vector<std::string>& map)
+  {
+    if (owner->onGround && currDirection == settings::unitsDirections::left) {
+      owner->moveLeft();
+    } else if (owner->onGround && currDirection == settings::unitsDirections::right) {
+     owner->moveRight();
+    }
+    
+    checkHazards(map);
+    detectPlayer();
+  }
+  
+
+  void Brain::checkHazards(const std::vector<std::string>& map)
+  {
+    if (currDirection == settings::unitsDirections::left) {
+      checkHazardsByLeftSide(map);
+    } else if (currDirection == settings::unitsDirections::right) {
+      checkHazardsByRightSide(map);
+    }
+  }
+
+
+  void Brain::checkHazardsByLeftSide(const std::vector<std::string>& map)
+  {
+    int tileSize = settings::sprite_resolution;
+    int x = owner->hitBox.left / tileSize;
+    int yMin = owner->hitBox.top / tileSize;
+    int yMax = (owner->hitBox.top + owner->hitBox.height) / tileSize + 1;
+
+    if (x >= 0) {
+      for (int y = yMin; y < yMax; y++) {
+        try {
+          char mapBlockLabel = map.at(y).at(x);
+
+          if (exist(settings::damageObjects, mapBlockLabel)) {
+            currDirection = settings::unitsDirections::right;
+          }
+
+        } catch (std::out_of_range err) {
+          std::cerr << err.what() << std::endl;
+          return;
+        }
+      }
+    }
+  }
+
+
+  void Brain::checkHazardsByRightSide(const std::vector<std::string>& map)
+  {
+    int tileSize = settings::sprite_resolution;
+
+    int x = (owner->hitBox.left + owner->hitBox.width) / tileSize;
+    int yMin = owner->hitBox.top / tileSize;
+    int yMax = (owner->hitBox.top + owner->hitBox.height) / tileSize + 1;
+
+    if (x < map.at(0).size()) {
+
+      for (int y = yMin; y < yMax; y++) {
+        try {
+          char mapBlockLabel = map.at(y).at(x);
+
+          if (exist(settings::damageObjects, mapBlockLabel)) {
+            currDirection = settings::unitsDirections::left;
+          }
+
+        } catch (std::out_of_range err) {
+          std::cerr << err.what() << std::endl;
+          return;
+        }
+      }
+    }
+  }
+
+
+  void Brain::detectPlayer()
+  {
+    int tileSize = settings::sprite_resolution;
+
+    int fovY = 2;
+    int fovX = 4;
+    int xMin;
+    int xMax;
+    int yMin;
+    int yMax;
+
+    const sf::FloatRect& playerHitbox = GameData::playerPtr->getHitbox();
+
+    int playerMinX = playerHitbox.left / tileSize;
+    int playerMaxX = (playerHitbox.left +  playerHitbox.width) / tileSize;
+    int playerMinY = playerHitbox.top / tileSize;
+    int playerMaxY = (playerHitbox.top +  playerHitbox.height) / tileSize;
+
+    const sf::FloatRect& ownerHitBox = owner->getHitbox();
+
+    if (currDirection == settings::unitsDirections::left) {
+      xMin = ownerHitBox.left / tileSize;
+      xMax = xMin - fovX;
+    } else if (currDirection == settings::unitsDirections::right) {
+      xMin = (ownerHitBox.left + ownerHitBox.width) / tileSize;
+      xMax = xMin + fovX;
+    }
+
+    yMin = ownerHitBox.top / tileSize - fovY;
+    yMax = (ownerHitBox.top + ownerHitBox.height) / tileSize + fovY;
+
+    for (int y = yMin; y < yMax; y++) {
+      for (int x = xMin; x < xMax; x++) {
+        try {
+          if ((x == playerMinX || x == playerMaxX) &&
+             (y == playerMinY || y == playerMaxY)) {
+            std::cout << owner->getName() + "'s dick is up\n";
+          }
+        } catch (std::out_of_range err) {
+          std::cerr << err.what() << std::endl;
+        }
+      }
+    }
+  }
+
+
+  void Brain::handleCollision()
+  {
+    //TODO
+  }
 }
--- sfml_choke/objects/Brain.hpp
+++ sfml_choke/objects/Brain.hpp
@@ -2,15 +2,33 @@
 #ifndef Brain_hpp
 #define Brain_hpp
 
-#include "BaseEnemy.hpp"
+//#include "Game.hpp"
+#include "GameData.hpp"
+#include "gameUnit.hpp"
+#include "../global.h"
 
 namespace GO {
+  
+  class GameUnit;
+  
   class Brain
   {
-    const GameUnit* owner;
+  private:
+    GameUnit* owner;
+    settings::unitsDirections currDirection = settings::unitsDirections::none;
+    enum mindStates {attack, patrol, stand};
+    int currMindState;
+    
+  protected:
+    void checkHazards(const std::vector<std::string>&);
+    void checkHazardsByLeftSide(const std::vector<std::string>&);
+    void checkHazardsByRightSide(const std::vector<std::string>&);
+    void handleCollision();
+    void detectPlayer();
     
   public:
-    Brain(const GameUnit* owner);
+    Brain(GameUnit* owner);
+    void think(const std::vector<std::string>&);
   };
 }
 
